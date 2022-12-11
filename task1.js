a228da605d8d3c33cbd46c3d2009636480dd0edd
Halyna Velychko <halyna_velychko@epam.com>
2022-12-11 18:12:15 +0100

    added standart in and standart out streams

---
+++ task1.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin, 
+    output: process.stdout,
+});
+
+const showReversedString = () => rl.on('line', (string) => {
+    console.log(
+      `Your reversed string is: ${ string.split('').reverse().join('') }\n`
+    );
+  
+    rl.close();
+  });
+
+showReversedString();
+  
