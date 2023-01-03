ff25df184642d8e25565e80b85a985dc77fb4548
Halyna Velychko <halyna_velychko@epam.com>
2023-01-03 17:43:55 +0100

    [hw-2] added validation with joi

--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import * as userController from '../controllers/users.contoller';
+import { UserSchema } from '../models/user.schema';
+import { validateUserSchema } from './middleware/validation.middleware';
 
 const routerUsers = express.Router();
 
 routerUsers.route('/')
   .get(userController.getAllUsers)
-  .post(userController.addUser)
+  .post(validateUserSchema(UserSchema), userController.addUser)
 
 routerUsers.route('/:id')
   .get(userController.getUserInfo)
-  .patch(userController.updateUserInfo)
+  .patch(validateUserSchema(UserSchema), userController.updateUserInfo)
   .delete(userController.deleteUser)
 
 export {
