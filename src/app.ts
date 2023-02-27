80b50a95d9b347e0dbf0a75db92d08f36d4c8679
Halyna_Velychko <Halyna.Velychko@Amway.com>
2023-02-27 09:46:54 +0100

    hw-5

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,38 @@
 import { Group } from './models/group.model';
 import { sequelize } from './data-access/data-access';
 import express, { Application } from 'express';
-
+import morgan from 'morgan';
+import { Request, Response, NextFunction } from 'express';
 import { routerUsers } from './routers/users.router';
 import { User } from './models/user.model';
 import { usersData } from './data/users.data';
 import { groupRouters } from './routers/group.router';
 import { groupData } from './data/group.data';
 import { UserGroup } from './models/user-group.model';
+import { ResponseError } from './helpers/error-handling.helper';
 
 const app: Application = express();
 const PORT = Number(process.env.PORT) || 8080;
 
 app.use(express.json());
+// Logger
+app.use(morgan('dev'));
+
+process
+  .on('unhandledRejection', (reason, p) => {
+    console.error(reason, 'Unhandled Rejection at Promise', p);
+  })
+  .on('uncaughtException', err => {
+    console.error(err, 'Uncaught Exception thrown');
+    process.exit(1);
+  });
+
+app.use((err: Error, _: Request, res: Response) => {
+    if (err instanceof ResponseError) {
+        res.status(err.statusCode).json({ message: err.message });
+    }
+    res.status(500).json({ message: err.message });
+});
 
 app.use('/api/users', routerUsers);
 app.use('/api/groups', groupRouters);
