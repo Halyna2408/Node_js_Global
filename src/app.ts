bc6a7afbb4562ad990959280dde31b4bf4e43534
Halyna_Velychko <Halyna.Velychko@Amway.com>
2023-03-07 09:22:12 +0100

    hw-5: added trycatch wrapper, added winston logger

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import { Group } from './models/group.model';
 import { sequelize } from './data-access/data-access';
 import express, { Application } from 'express';
-import morgan from 'morgan';
 import { Request, Response, NextFunction } from 'express';
 import { routerUsers } from './routers/users.router';
 import { User } from './models/user.model';
@@ -10,28 +9,30 @@ import { groupRouters } from './routers/group.router';
 import { groupData } from './data/group.data';
 import { UserGroup } from './models/user-group.model';
 import { ResponseError } from './helpers/error-handling.helper';
+import { logger } from './helpers/logger.helper';
 
 const app: Application = express();
 const PORT = Number(process.env.PORT) || 8080;
 
 app.use(express.json());
-// Logger
-app.use(morgan('dev'));
 
 process
-  .on('unhandledRejection', (reason, p) => {
-    console.error(reason, 'Unhandled Rejection at Promise', p);
+  .on('unhandledRejection', (reason) => {
+    logger.error('Unhandled Rejection at Promise', reason);
   })
   .on('uncaughtException', err => {
-    console.error(err, 'Uncaught Exception thrown');
+    logger.error('Uncaught Exception thrown', err);
     process.exit(1);
   });
 
-app.use((err: Error, _: Request, res: Response, __: NextFunction) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  logger.error('Response err', err)
     if (err instanceof ResponseError) {
         res.status(err.statusCode).json({ message: err.message });
+        logger.error(`${err.statusCode || 404} - ${res.statusMessage} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`)
     }
     res.status(500).json({ message: err.message });
+    logger.error(`500 - ${res.statusMessage} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`)
 });
 
 app.use('/api/users', routerUsers);
@@ -56,5 +57,5 @@ sequelize.authenticate()
 .catch((error: Error) => console.log(`Oops! Something wents wrong: ${ error }`));
 
 app.listen(PORT, (): void => {
-  console.log(`Server Running on https://localhost:${PORT}`);
+  logger.info(`Server Running on https://localhost:${PORT}`);
 });
--- src/routers/group.router.ts
+++ src/routers/group.router.ts
@@ -1,20 +1,21 @@
 import express from 'express';
 import * as groupController from '../controllers/group.controller';
+import { tryCatchWrapper } from '../helpers/try-catch-wrapper.helper';
 
 const groupRouters = express.Router();
 
 groupRouters.route('/')
-  .get(groupController.getAllGroups)
-  .post(groupController.createGroup)
+  .get(tryCatchWrapper(groupController.getAllGroups))
+  .post(tryCatchWrapper(groupController.createGroup))
 
 groupRouters.route('/:groupId')
-  .get(groupController.getGroupById)
-  .patch(groupController.updateGroupById)
-  .delete(groupController.deleteGroup)
+  .get(tryCatchWrapper(groupController.getGroupById))
+  .patch(tryCatchWrapper(groupController.updateGroupById))
+  .delete(tryCatchWrapper(groupController.deleteGroup))
 
 groupRouters.route('/:groupId/add-users')
-  .post(groupController.addUsersToGroup)
-  .get(groupController.countUsersInGroup)
+  .post(tryCatchWrapper(groupController.addUsersToGroup))
+  .get(tryCatchWrapper(groupController.countUsersInGroup))
 
 export {
   groupRouters,
